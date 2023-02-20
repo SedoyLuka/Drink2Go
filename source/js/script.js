630cdaf62997a85437bc89f50977ceff35789193
SedoyLuka <lukasedoy@gmail.com>
2023-02-20 04:27:19 +0300

    Грейдирование

---
+++ source/js/script.js
@@ -0,0 +1,104 @@
+let navMain = document.querySelector('.main-nav');
+let navToggle = document.querySelector('.main-nav__toggle');
+
+navMain.classList.remove('main-nav--nojs');
+
+navToggle.addEventListener('click', function () {
+  if (navMain.classList.contains('main-nav--closed')) {
+    navMain.classList.remove('main-nav--closed');
+    navMain.classList.add('main-nav--opened');
+  } else {
+    navMain.classList.add('main-nav--closed');
+    navMain.classList.remove('main-nav--opened');
+  }
+});
+
+const prev = document.getElementById('btn-prev'),
+    next = document.getElementById('btn-next'),
+    slides = document.querySelectorAll('.slider__item'),
+    dots = document.querySelectorAll('.slider__pagination-dot');
+
+let index = 0;
+
+const activeSlide = n => {
+    for(slide of slides) {
+        slide.classList.remove('slider__item--active');
+    }
+    slides[n].classList.add('slider__item--active');
+};
+
+const activeDot = n => {
+    for(dot of dots) {
+        dot.classList.remove('slider__pagination-dot--active');
+    }
+    dots[n].classList.add('slider__pagination-dot--active');
+};
+
+const prepareCurrentSlide = ind => {
+    activeSlide(index);
+    activeDot(index);
+};
+
+const nextSlide = () => {
+    if(index == slides.length - 1) {
+        index = 0;
+        prepareCurrentSlide(index);
+    } else {
+    index++;
+    prepareCurrentSlide(index);
+    }
+};
+
+const prevSlide = () => {
+    if(index == 0) {
+        index = slides.length - 1;
+        prepareCurrentSlide(index);
+    } else {
+    index--;
+    prepareCurrentSlide(index);
+    }
+};
+
+dots.forEach((item, indexDot) => {
+    item.addEventListener('click', () => {
+        index = indexDot;
+        prepareCurrentSlide(index);
+    })
+});
+
+next.addEventListener('click', nextSlide);
+prev.addEventListener('click', prevSlide);
+
+
+//LEAFLET
+
+const map = L.map('map-wrapper')
+  .setView({
+    lat: 59.968137,
+    lng: 30.316272,
+  }, 16);
+
+L.tileLayer(
+  'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+  {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  },
+).addTo(map);
+
+const mainPinIcon = L.icon({
+  iconUrl: './img/map/map-pin.png',
+  iconSize: [38, 50],
+  iconAnchor: [19, 50],
+});
+
+const marker = L.marker(
+  {
+    lat: 59.968137,
+    lng: 30.316272,
+  },
+  {
+    icon: mainPinIcon,
+  },
+);
+
+marker.addTo(map);
